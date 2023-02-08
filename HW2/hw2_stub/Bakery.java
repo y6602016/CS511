b66e8153607d015f2450ae48d35d791a79e325dd
Mike Hong <y6602016@gmail.com>
2023-02-08 14:02:46 -0500

    add HW2

---
+++ HW2/hw2_stub/Bakery.java
@@ -0,0 +1,55 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.CountDownLatch;
+
+public class Bakery implements Runnable {
+    private static final int TOTAL_CUSTOMERS = 200;
+    private static final int CAPACITY = 50;
+    private static final int FULL_BREAD = 20;
+    private Map<BreadType, Integer> availableBread;
+    private ExecutorService executor;
+    private float sales = 0;
+    private CountDownLatch doneSignal = new CountDownLatch(TOTAL_CUSTOMERS);
+    // TODO
+
+    /**
+     * Remove a loaf from the available breads and restock if necessary
+     */
+    public void takeBread(BreadType bread) {
+        int breadLeft = availableBread.get(bread);
+        if (breadLeft > 0) {
+            availableBread.put(bread, breadLeft - 1);
+        } else {
+            System.out.println("No " + bread.toString() + " bread left! Restocking...");
+            // restock by preventing access to the bread stand for some time
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+            availableBread.put(bread, FULL_BREAD - 1);
+        }
+    }
+
+    /**
+     * Add to the total sales
+     */
+    public void addSales(float value) {
+        sales += value;
+    }
+
+    /**
+     * Run all customers in a fixed thread pool
+     */
+    public void run() {
+        availableBread = new ConcurrentHashMap<BreadType, Integer>();
+        availableBread.put(BreadType.RYE, FULL_BREAD);
+        availableBread.put(BreadType.SOURDOUGH, FULL_BREAD);
+        availableBread.put(BreadType.WONDER, FULL_BREAD);
+
+        // TODO
+    }
+}
---
+++ HW2/hw2_stub/Customer.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class Customer implements Runnable {
+    private Bakery bakery;
+    private Random rnd;
+    private List<BreadType> shoppingCart;
+    private int shopTime;
+    private int checkoutTime;
+    private CountDownLatch doneSignal;
+    
+    /**
+     * Initialize a customer object and randomize its shopping cart
+     */
+    public Customer(Bakery bakery, CountDownLatch l) {
+        // TODO
+    }
+
+    /**
+     * Run tasks for the customer
+     */
+    public void run() {
+        // TODO
+    }
+
+    /**
+     * Return a string representation of the customer
+     */
+    public String toString() {
+        return "Customer " + hashCode() + ": shoppingCart=" + Arrays.toString(shoppingCart.toArray()) + ", shopTime=" + shopTime + ", checkoutTime=" + checkoutTime;
+    }
+
+    /**
+     * Add a bread item to the customer's shopping cart
+     */
+    private boolean addItem(BreadType bread) {
+        // do not allow more than 3 items, chooseItems() does not call more than 3 times
+        if (shoppingCart.size() >= 3) {
+            return false;
+        }
+        shoppingCart.add(bread);
+        return true;
+    }
+
+    /**
+     * Fill the customer's shopping cart with 1 to 3 random breads
+     */
+    private void fillShoppingCart() {
+        int itemCnt = 1 + rnd.nextInt(3);
+        while (itemCnt > 0) {
+            addItem(BreadType.values()[rnd.nextInt(BreadType.values().length)]);
+            itemCnt--;
+        }
+    }
+
+    /**
+     * Calculate the total value of the items in the customer's shopping cart
+     */
+    private float getItemsValue() {
+        float value = 0;
+        for (BreadType bread : shoppingCart) {
+            value += bread.getPrice();
+        }
+        return value;
+    }
+}
